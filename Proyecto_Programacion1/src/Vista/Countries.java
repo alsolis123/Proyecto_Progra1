b6ae412b6a7de5ae60b0c2d81c3ae1eb10bcf6ac
Alberto Solis <alberto.solis@ni.com>
2021-04-06 20:56:39 -0600

    Inicio de Countries

---
+++ Proyecto_Programacion1/src/Vista/Countries.java
@@ -0,0 +1,45 @@
+
+package Vista;
+
+
+public class Countries {
+    private String url;
+    private int count;
+    private int sCode;
+
+    public Countries(String url, int count, int sCode) {
+        this.url = "";
+        this.count = 0;
+        this.sCode = 0;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getsCode() {
+        return sCode;
+    }
+
+    public void setsCode(int sCode) {
+        this.sCode = sCode;
+    }
+    
+    
+    
+    
+    
+    
+}
